41c9468d781ecf56e43c35486537d2f5da9f301a
hyungilee <mike.hyungi.lee@gmail.com>
2020-01-15 20:58:34 +0900

    lesson 8

--- app/src/main/java/com/example/swoosh/Constants.kt
+++
@@ -1,7 +0,0 @@
-package com.example.swoosh
-
-/**
- * Created by HyungiLee on 01/14/20
- * */
-
-const val EXTRA_LEAGUE = "league"
---
+++ app/src/main/java/com/example/swoosh/Utilities/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.swoosh.Utilities
+
+/**
+ * Created by HyungiLee on 01/14/20
+ * */
+
+const val EXTRA_LEAGUE = "league"
+const val EXTRA_SKILL = "skill"
